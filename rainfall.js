94d1c70f37a811d0d2eb07ba7262ff3dd92c1d19
muhammad.rafay <muhammad.rafay@rolustech.net>
2021-08-01 15:45:21 +0500

    Intial Changes

--- rainfall.js
+++ rainfall.js
@@ -1,10 +1,15 @@
 
 
 
-data = await d3.csv('Rainfall_1901_2016_PAK.csv');
+async function loadData() {
+
+    d = await d3.csv('Rainfall_1901_2016_PAK.csv');
+    return d;
+}
+
+var data = loadData()
 
 var grouped_data = d3.group(data, d => d.Year)
-
 
 console.log(grouped_data)
 
