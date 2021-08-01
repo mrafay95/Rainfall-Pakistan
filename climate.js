ca73f6578283e9ebf571914d8457ecd7dbcd4469
muhammad.rafay <muhammad.rafay@rolustech.net>
2021-08-01 23:54:32 +0500

    Intial Changes

--- climate.js
+++ climate.js
@@ -1,6 +1,9 @@
 
 var data;
-var grouped_data
+var grouped_data;
+
+var Country = 'World';
+var Year = '2018'
 
 async function loadData() {
 
@@ -31,6 +34,25 @@ loadData().then(() => {
 function co2ForestScene() {
 
     console.log(data)
+
+    Country_filter = _.where(data, {"Country Name": Country});
+
+    CO2_filter = _.where(Country_filter, {"Series Name": "CO2 emissions (kt)"});
+    Forest_filter = _.where(Country_filter, {"Series Name": "Forest area (sq. km)"});
+
+    C02_array = _.pluck(CO2_filter, Year);
+    Forest_array = _.pluck(CO2_filter, Year);
+    CountryName_array = _.pluck(CO2_filter, 'Country Name');
+
+
+    var co2Forest = []
+    for (let index = 0; index < CountryName_array.length; index++) {
+        co2Forest.push({'Country': CountryName_array[index], 'C02': C02_array[index], 'Forest': Forest_array[index]})
+        
+    }
+
+    console.log(co2Forest)
+
 
 }
 
