054f502f37a17ef336944d896d9bca12617c6eb3
muhammad.rafay <muhammad.rafay@rolustech.net>
2021-08-02 02:51:57 +0500

    Intial Changes

--- climate.js
+++ climate.js
@@ -59,8 +59,8 @@ loadData().then(() => {
 
 function co2ForestScene() {
 
-    var x = d3.scaleLinear().domain([0,10000000]).range([ 0, width ])
-    var y = d3.scaleLinear().domain([0,10000000]).range([ height, 0])
+    var x = d3.scaleLinear().domain([1000,10000000]).range([ 0, width ])
+    var y = d3.scaleLinear().domain([100000,10000000]).range([ height, 0])
 
     //Country_filter = _.where(data, {"Country Name": Country});
 
@@ -89,7 +89,6 @@ function co2ForestScene() {
 
 
     svg.append('g')
-    .attr("transform","translate("+50+","+50+")")
     .selectAll('dot')
     .data(co2Forest)
     .enter()
